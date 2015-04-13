aa4907300b80b3128a42761dc974f4e2e88b5e71
Greg Marut <greg.marut@gmail.com>
2015-04-13 10:14:10 -0400

    Added test cases for inheritance

---
+++ src/test/java/com/gregmarut/support/bean/ChildBean.java
@@ -0,0 +1,16 @@
+package com.gregmarut.support.bean;
+
+public class ChildBean extends ParentBean
+{
+	private int childValue;
+	
+	public int getChildValue()
+	{
+		return childValue;
+	}
+	
+	public void setChildValue(int childValue)
+	{
+		this.childValue = childValue;
+	}
+}
