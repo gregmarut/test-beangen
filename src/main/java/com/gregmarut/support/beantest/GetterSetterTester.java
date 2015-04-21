98fbe5527309287fb60af92ce5a0c33fd201d62f
Greg Marut <greg.marut@gmail.com>
2015-04-21 10:57:23 -0400

    Updated copyright headers in java files

--- src/main/java/com/gregmarut/support/beantest/GetterSetterTester.java
+++ src/main/java/com/gregmarut/support/beantest/GetterSetterTester.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
  * <pre>
- * Copyright (c) 2015 Greg.
+ * Copyright (c) 2015 Greg Marut.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
  * 
  * Contributors:
- *     Greg - initial API and implementation
+ *     Greg Marut - initial API and implementation
  * </pre>
  ******************************************************************************/
 package com.gregmarut.support.beantest;
